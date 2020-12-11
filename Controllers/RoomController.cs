10a1d4b323a81adc8ad66fe0449d2fdbc3fe389e
Mykty-Ayan <akkassov.ayan@gmail.com>
2020-12-11 07:57:42 +0600

    Initial Commit

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Hotel.Models;
+
+namespace Hotel.Controllers
+{
+    public class RoomController : Controller
+    {
+        private RoomsDBcontext1 db = new RoomsDBcontext1();
+
+        // GET: Room
+        public ActionResult Index()
+        {
+            
+            return View(db.Rooms.ToList());
+            
+        }
+
+        public ActionResult Searched(int capacity, DateTime dateFrom, DateTime dateTo)
+        {
+           
+            if (dateFrom <= dateTo)
+            {
+                var rooms = db.Rooms.Where(x => (x.Capacity >= capacity) && (x.DateFrom <= dateFrom) && (x.DateTo >= dateTo)).ToList();
+                return View(rooms);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+            
+
+        }
+
+        // GET: Room/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // GET: Room/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Room/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Capacity,Price,DateFrom,DateTo")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rooms.Add(room);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(room);
+        }
+
+        // GET: Room/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Room/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Capacity,Price,DateFrom,DateTo")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(room);
+        }
+
+        // GET: Room/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Room/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateFrom { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateTo { get; set; }
+
+
+    }
+
+    class RoomsDBcontext1 : DbContext
+    {
+        public RoomsDBcontext1()
+        {
+
+        }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
